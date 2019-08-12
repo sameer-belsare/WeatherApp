cd36497bb13f17e1eed3cf3d19dc1dcf4a413186
sameerb-webonise <sameer.belsare@weboniselab.com>
2019-08-12 11:10:34 +0530

    required package creation

--- app/src/main/java/com/example/weatherapp/ui/story/weather/WeatherActivity.kt
+++ app/src/main/java/com/example/weatherapp/ui/story/weather/WeatherActivity.kt
@@ -3,7 +3,7 @@ package com.example.weatherapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class WeatherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
